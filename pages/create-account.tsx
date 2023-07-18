0acada10437c81d62af2043cf8a7c2dc9e5ad0ea
sohyun <shlim0410@gmail.com>
2023-07-18 15:54:14 +0100

    setup: basic router setting

---
+++ pages/create-account.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CreateAccount() {
+  return (
+    <div>
+      <h1>Craete Account page</h1>
+    </div>
+  );
+}
---
+++ pages/tweet/[id].tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tweet() {
+  return (
+    <div>
+      <h1>every single tweet with id</h1>
+    </div>
+  );
+}

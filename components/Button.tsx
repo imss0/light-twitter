2a7fb41e773aae6b576cf330326d285cbbbf55a9
Sohyun Lim <shlim0410@gmail.com>
2023-07-25 08:22:11 +0000

    Update 5 files

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,7 @@
 interface ButtonProps {
   size?: "default" | "large";
   text: string;
+  disabled: boolean;
   [key: string]: any;
 }
 
@@ -12,12 +13,19 @@ const buttonSizes = {
 export default function Button({
   size = "default",
   text,
+  disabled,
   ...rest
 }: ButtonProps) {
   return (
     <button
       {...rest}
-      className={`${buttonSizes[size]} m-1 rounded-md bg-blue-400 text-white cursor-pointer hover:bg-blue-600`}
+      className={`${
+        buttonSizes[size]
+      } m-1 rounded-md bg-blue-400 text-white cursor-pointer hover:bg-blue-600 ${
+        disabled
+          ? "bg-gray-400 text-gray-600 hover:bg-gray-300 cursor-not-allowed"
+          : "bg-blue-400 text-white cursor-pointer hover:bg-blue-600"
+      }`}
     >
       {text}
     </button>
